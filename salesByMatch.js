1f37d2da22d931af5dffc05e9e432b6b21c5126a
throne.yash@gmail.com <throne.yash@gmail.com>
2021-12-07 20:32:05 -0700

    Adding salesByMatch problem

---
+++ salesByMatch.js
@@ -0,0 +1,64 @@
+// There is a large pile of socks that must be paired by color. Given an array of integers representing the color of each sock, determine how many pairs of socks with matching colors there are.
+
+// Example
+
+// There is one pair of color  and one of color . There are three odd socks left, one of each color. The number of pairs is .
+
+// Function Description
+
+// Complete the sockMerchant function in the editor below.
+
+// sockMerchant has the following parameter(s):
+
+// int n: the number of socks in the pile
+// int ar[n]: the colors of each sock
+// Returns
+
+// int: the number of pairs
+// Input Format
+
+// The first line contains an integer , the number of socks represented in .
+// The second line contains  space-separated integers, , the colors of the socks in the pile.
+
+// Constraints
+
+//  where
+// Sample Input
+
+// STDIN                       Function
+// -----                       --------
+// 9                           n = 9
+// 10 20 20 10 10 30 50 10 20  ar = [10, 20, 20, 10, 10, 30, 50, 10, 20]
+// Sample Output
+
+// 3
+// Explanation
+
+// sock.png
+
+// There are three pairs of socks.
+
+/*
+ * Complete the 'sockMerchant' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER_ARRAY ar
+ */
+
+function sockMerchant(n, ar) {
+  // Write your code here
+  let count = 0
+  let hashMap = ar.reduce((a, c) => {
+    a[c] = a[c] + 1 || 1
+    return a
+  }, {})
+
+  for (const sock in hashMap) {
+    count += Math.floor(hashMap[sock] / 2)
+  }
+  return count
+}
+
+console.log(sockMerchant(9, [10, 20, 20, 10, 10, 30, 50, 10, 20])) // 3
